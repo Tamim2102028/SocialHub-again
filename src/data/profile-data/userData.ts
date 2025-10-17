0cfe4271ffb805be132143d11471813748b34c33
Md. Meshbaul Haque Tamim <2102028@che.buet.ac.bd>
2025-10-17 15:07:15 +0600

    good

--- src/data/profile-data/userData.ts
+++ src/data/profile-data/userData.ts
@@ -59,7 +59,7 @@ export const usersData: UserData[] = [
     category: "university",
     university: {
       name: "BUET",
-      dept: "Computer Science & Engineering bro",
+      dept: "Computer Science & Engineering bro hard",
       section: "A",
       subsection: "1",
       roll: "1905001",
